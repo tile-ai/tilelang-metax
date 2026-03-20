cf8563e6aca603ca0f2e2ab34c4c2aa2e4f29d2b
Five-HZ <huanzhou.wu@metax-tech.com>
2026-03-20 03:01:16 +0000

    [CI][Examples] adaptation of examples cases for MACA target

--- examples/maca/deepseek_mhc/test_example_mhc.py
+++ examples/maca/deepseek_mhc/test_example_mhc.py
@@ -4,12 +4,10 @@ from example_mhc_post import main as main_post
 from example_mhc_pre import main as main_pre
 
 
-@tilelang.testing.pytest.mark.xfail
 def test_mhc_post():
     main_post()
 
 
-@tilelang.testing.pytest.mark.xfail
 def test_mhc_pre():
     main_pre()
 
