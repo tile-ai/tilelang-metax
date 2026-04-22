d88793d0c25c14c2878cc1ee48fa91850f827b16
suwei.ye <suwei.ye@metax-tech.com>
2026-04-22 10:55:52 +0800

    [MetaxGPU][examples] add template function  ToHalf2(float2 val)

--- examples/deepseek_nsa/test_example_tilelang_nsa.py
+++ examples/deepseek_nsa/test_example_tilelang_nsa.py
@@ -3,6 +3,7 @@ import tilelang.testing
 
 from example_tilelang_nsa_fwd import main as main_fwd
 from example_tilelang_nsa_decode import main as main_fwd_decode
+from example_tilelang_nsa_bwd import main as main_bwd
 
 
 def test_example_tilelang_nsa_fwd():
@@ -11,6 +12,10 @@ def test_example_tilelang_nsa_fwd():
 
 def test_example_tilelang_nsa_fwd_decode():
     main_fwd_decode()
+
+
+def test_example_tilelang_nsa_bwd():
+    main_bwd()
 
 
 if __name__ == "__main__":
